a03e1fa09514ed8d70030b853caa32870094714c
Brib16 <152118254+Brib16@users.noreply.github.com>
2023-12-07 15:33:56 -0600

    Update PlaylistNode.cpp

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -49,3 +49,56 @@ using namespace std;
            current = next;
     }
 }
+PlaylistNode* PlaylistNode::RemoveSong(PlaylistNode* headNode, const std::string& uniqueID) {
+    PlaylistNode* current = headNode;
+    PlaylistNode* previous = nullptr;
+
+    // Traverse the linked list to find the node with the specified uniqueID
+    while (current != nullptr && current->GetID() != uniqueID) {
+        previous = current;
+        current = current->GetNext();
+    }
+
+    // If the node with the specified uniqueID is found
+    if (current != nullptr) {
+        // Print the information before removing the node
+        cout << "\"" << current->GetSongName() << "\" removed." << endl<< endl;
+
+        // Update the pointers to remove the node
+        if (previous != nullptr) {
+            previous->SetNext(current->GetNext());
+        } else {
+            // If the node to be removed is the head, update the head
+            headNode = current->GetNext();
+        }
+
+        // Free the memory of the removed node
+        delete current;
+    } else {
+        cout << "Song with ID \"" << uniqueID << "\" not found." << endl;
+    }
+
+    return headNode;  // Return the updated headNode
+}
+void PlaylistNode::OutputSongsByArtist(const std::string& artistName) const {
+    const PlaylistNode* current = this;
+    int position = 1;
+
+    while (current != nullptr) {
+        if (current->GetArtistName() == artistName) {
+            std::cout << position << ".\n";
+            current->PrintPlaylistNode();
+            std::cout << std::endl;
+        }
+
+        current = current->GetNext();
+        position++;
+    }
+}
+// Other member functions
+void PlaylistNode::PrintPlaylistNode() const {
+    std::cout << "Unique ID: " << GetID() << std::endl;
+    std::cout << "Song Name: " << GetSongName() << std::endl;
+    std::cout << "Artist Name: " << GetArtistName() << std::endl;
+    std::cout << "Song Length (in seconds): " << GetSongLength() << std::endl; //<< std::endl;
+}
